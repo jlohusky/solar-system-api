452baf33394f36ae9f63fc3d7304afa94deb041a
Julie Loh <jlohusky@gmail.com>
2023-04-20 11:46:05 -0700

    created endpoint to handle GET all planets and registered planet_bp

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify
 
 class Planet():
     def __init__(self, id, name, description, is_planet):
@@ -10,7 +10,13 @@ class Planet():
 planets = [
     Planet(1, "Earth", "We live here.", True),
     Planet(2, "Mercury", "The smallest planet.", True),
-    Planet(3, "Mars", "Dusty cold desert world.", True)
+    Planet(3, "Mars", "Dusty cold desert world.", True),
     Planet(4, "Pluto", "Dwarf planet.", False)
 ]
 
+planets_bp = Blueprint("planets", __name__, url_prefix="/planets")
+
+@planets_bp.route("", methods=['GET'])
+def handle_planets():
+    planets_as_dict = [vars(planet) for planet in planets]
+    return jsonify(planets_as_dict), 200
