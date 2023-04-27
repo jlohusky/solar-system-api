8d0fb0317a214e16daf37df7450718384ede85ea
Julie Loh <jlohusky@gmail.com>
2023-04-27 14:23:48 -0700

    deleted old Planet class and list of Planet instances

--- app/routes.py
+++ app/routes.py
@@ -2,20 +2,6 @@ from flask import Blueprint, jsonify, abort, make_response, request
 from app.models.planet import Planet
 from app import db
 
-# class Planet():
-#     def __init__(self, id, name, description, is_planet):
-#         self.id = id
-#         self.name = name
-#         self.description = description
-#         self.is_planet = is_planet
-
-# planets = [
-#     Planet(1, "Earth", "We live here.", True),
-#     Planet(2, "Mercury", "The smallest planet.", True),
-#     Planet(3, "Mars", "Dusty cold desert world.", True),
-#     Planet(4, "Pluto", "Dwarf planet.", False)
-# ]
-
 def validate_planet(planet_id):
     try:
         planet_id = int(planet_id)
