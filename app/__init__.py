2ad897a906d73445ef3eddd14c99cf7eb861010a
Julie Loh <jlohusky@gmail.com>
2023-05-02 11:39:19 -0700

    added testing environ.get to app/__init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,16 +1,20 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from dotenv import load_dotenv
+import os
 
 db = SQLAlchemy()
 migrate = Migrate()
+load_dotenv()
 
-
-def create_app(test_config=None):
+def create_app(testing=None):
     app = Flask(__name__)
 
-    # SQLALCHEMY is specifically looking for these variables
-    app.config["SQLALCHEMY_DATABASE_URI"] = 'postgresql+psycopg2://postgres:postgres@localhost:5432/solar_system_development'
+    if testing is None:
+        app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get('DEV_DATABASE_URI')
+    else:
+        app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get('TEST_DATABASE_URI')
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.init_app(app)
     migrate.init_app(app, db)
